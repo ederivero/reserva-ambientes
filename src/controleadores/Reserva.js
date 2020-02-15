51c3768f6059fd623711cf6145a3d6cb33568c0a
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2020-02-14 19:55:33 -0500

    agrege modelos ambiente y pabellon en reservas

--- src/controleadores/Reserva.js
+++ src/controleadores/Reserva.js
@@ -111,7 +111,16 @@ const getReservaByFechas = (req,res)=>{
     })
 }
 const getReservas=(req,res)=>{
-    Reserva.findAll().then(reservas=>{
+    Reserva.findAll({
+        include:{
+            model: Ambiente,
+            attributes:['amb_nro'],
+            include:{
+                model:Pabellon,
+                attributes:['pab_nom']
+            }
+        }
+    }).then(reservas=>{
         res.status(200).json({
             ok: true,
             contenido: reservas
